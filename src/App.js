749a25256a1a4d991ee916ae7fab8bbd9b7b4c4d
Susovan <susovan.sahoo777@gmail.com>
2024-07-29 12:12:10 +0530

    Done the project building

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import { Outlet } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import Footer from "./components/Footer/Footer";
+import DownloadApp from "./components/Sections/DownloadApp/DownloadApp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div>
+      <CssBaseline />
+      <Outlet />
+      <DownloadApp />
+      <Footer />
     </div>
   );
 }
